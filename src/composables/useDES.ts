4418e368e03e405209888ea88c82af70be1bd771
lukasz_hossa <lukasz.hossa15@gmail.com>
2022-05-15 16:54:29 +0200

    add decrypt

--- src/composables/useDES.ts
+++ src/composables/useDES.ts
@@ -37,7 +37,6 @@ export default (key = 108755180518133317n) => {
 
       keys.push(permutate([...new BitArray(keyC, 28), ...new BitArray(keyD, 28)], PC2))
     }
-
     const result = []
     for (const [left, right] of blockPairs32) {
       const newBlocks: [BitArray, BitArray][] = []
@@ -76,6 +75,61 @@ export default (key = 108755180518133317n) => {
 
   const decrypt = async (file: MaybeRef<File>) => {
 
+    const blocks = await use64BitBlocks(get(file))
+    const blockPairs32 = [...blocks].map(block => {
+      const bitBlock = permutate(new BitArray(block), IP)
+      return cast64BitBlock(Uint32Array, BigInt(`0b${bitBlock.join('')}`))
+    })
+
+    const permutedChoice = permutate(cast64BitBlock(BitArray, key), PC)
+    let keyC = parseInt(permutedChoice.slice(0, 28).join(''), 2)
+    let keyD = parseInt(permutedChoice.slice(28).join(''), 2)
+
+    const keys = []
+    for (let i = 0; i < SHIFT.length; i++) {
+      keyC <<= SHIFT[i]
+      keyC &= 0xfffffff
+      keyD <<= SHIFT[i]
+      keyD &= 0xfffffff
+
+      keys.push(permutate([...new BitArray(keyC, 28), ...new BitArray(keyD, 28)], PC2))
+    }
+    const reversedKeys = keys.reverse()
+
+    const result = []
+    for (const [left, right] of blockPairs32) {
+      const newBlocks: [BitArray, BitArray][] = []
+
+      const rightBitArray = new BitArray(right)
+      const leftBitArray = new BitArray(left)
+
+      for (let i = 0; i < SHIFT.length; i++) {
+        const key = reversedKeys[i]
+
+        const rightExtended = permutate(rightBitArray, E)
+          .map((bit, i) => bit ^ key[i])
+
+        const bits32 = [...Array(8).keys()].map(i => {
+          return rightExtended.slice(0 + 6 * i, 6 + 6 * i)
+        }).map(([y1, x1, x2, x3, x4, y2], i) => {
+          const x = parseInt([x1, x2, x3, x4].join(''), 2)
+          const y = parseInt([y1, y2].join(''), 2)
+          return new BitArray(S[i][y][x], 4)
+        }).reduce((acc: number[], a) => [...acc, ...a], [])
+
+        newBlocks.push([
+          rightBitArray, 
+          permutate(bits32, P).map((bit, i) => bit ^ leftBitArray[i])
+        ])
+      }
+
+      const [leftBlock, rightBlock] = newBlocks[SHIFT.length - 1]
+      result.push(BigInt(`0b${permutate([...rightBlock, ...leftBlock], IIP).join('')}`))
+    }
+
+    return new File([
+      new BigUint64Array(result).buffer
+    ], 'decrypted.bin', { type: 'application/octet-stream' })
   }
 
   return {
